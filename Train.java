e987950ae2cfacfad8c67d687d2b32563d662965
emmafoley <emmaf818@gmail.com>
2016-02-26 13:27:24 -0500

    Update Train.java

--- Train.java
+++ Train.java
@@ -1,13 +1,23 @@
 public class Train
 {
   private String type;
-  private Color color;
+  private int x;
+  private int y;
+  public Color color;
+  
+ public Train(int startX,int startY)
+ {
+     startX = x;
+     startY = y;
+ }
  
+ public Train(Color color,int x,int y)
+ {
+   this.color = color;
+ }
   public void addCar(String type,Color color)
   {
-    this.type = type;
-    this.color = color;
-  
+
   }
   public void showCars()
   {
