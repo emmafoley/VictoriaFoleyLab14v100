5da6bc74553c280212d3458beb6a8bf279bd7b4c
emmafoley <emmaf818@gmail.com>
2016-03-01 18:41:33 -0500

    Update Train.java

--- Train.java
+++ Train.java
@@ -9,14 +9,15 @@ public class Train
   private int y;
   private int pos;
   private Color color;
-  private ArrayList<RailCar> train = new ArrayList<RailCar>();    // construct new ArrayList train
+  private ArrayList<RailCar> train;    // construct new ArrayList train
   
  public Train(int x,int y)    // train constructor
  {
      this.x = x;
      this.y = y;
-     this.color = color;
      this.pos = pos;
+     this.color = color;
+     train = new ArrayList<RailCar>();
  }
  
 
@@ -33,41 +34,31 @@ public class Train
          train.add(cab);
          }
       if (type.equals("PassengerCar")) {
-         PassengerCar pass = new PassengerCar(color,x,y);
+         PassengerCar pc = new PassengerCar(color,x,y);
          x += 175;
-         train.add(pass);
+         train.add(pc);
          }
       if (type.equals("FreightCar")) {
-         FreightCar fre = new FreightCar(color,x,y);
+         FreightCar fc = new FreightCar(color,x,y);
          x += 175;
-         train.add(fre);
+         train.add(fc);
       }
       
    }
    
        public void addCar(int pos,String type,Color color)     // overloaded addCar method, included position
    {
-      if (type.equals("Locomotive")) {
-         Locomotive loco = new Locomotive(color,x,y);
-         x += 175;
-         train.add(pos,loco);    // overloaded add method inserts car at position
-         }
-      if (type.equals("Caboose")) {
-         Caboose cab = new Caboose(color,x,y);
-         x += 175;
-         train.add(pos,cab);
-         }
       if (type.equals("PassengerCar")) {
-         PassengerCar pass = new PassengerCar(color,x,y);
+         PassengerCar pass = new PassengerCar(color,x,y);   
+         train.add(3,pass);
          x += 175;
-         train.add(pos,pass);
          }
-      if (type.equals("FreightCar")) {
+      else {
          FreightCar fre = new FreightCar(color,x,y);
+         train.add(6,fre);
          x += 175;
-         train.add(pos,fre);}  
-   }
-   
+         }
+   }   
    
    public void showCars(Graphics g)
    {
