fc032c24789bf80ab5204c0676684b192fbb1ca2
emmafoley <emmaf818@gmail.com>
2016-03-01 09:38:51 -0500

    Update Train.java

--- Train.java
+++ Train.java
@@ -7,71 +7,73 @@ public class Train
   private String type;
   private int x;
   private int y;
+  private int pos;
   private Color color;
-  private ArrayList<RailCar> train = new ArrayList<RailCar>();    // create train ArrayList
+  private ArrayList<RailCar> train = new ArrayList<RailCar>();
   
- public Train(int x,int y)    // train class constructor
+ public Train(int x,int y)
  {
      this.x = x;
      this.y = y;
      this.color = color;
+     this.pos = pos;
  }
  
-   public void addCar(String type,Color color)
-   {
-      if (type.equals("Locomotive")) {                        // for each type of car
-         Locomotive loco = new Locomotive(color,x,y);         // create new car object
-         x += 150;                                            // move over
-         train.add(loco);                                     // add object to ArrayList
-         }
-      if (type.equals("Caboose")) {
-         Caboose cab = new Caboose(color,x,y);
-         x += 150;
-         train.add(cab);
-         }
-      if (type.equals("PassengerCar")) {
-         PassengerCar pass = new PassengerCar(color,x,y);
-         x += 150;
-         train.add(pass);
-         }
-      if (type.equals("FreightCar")) {
-         FreightCar fre = new FreightCar(color,x,y);
-         x += 150;
-         train.add(fre);
-      }
-      
-   
-   }
-   
-   public void addCars(int pos,String type,Color color)
+    public void addCars(int pos,String type,Color color)
    {
     if (type.equals("Locomotive")) {
          Locomotive loco = new Locomotive(color,x,y);
-         x += 150;
+         x += 175;
          train.add(pos,loco);
          }
       if (type.equals("Caboose")) {
          Caboose cab = new Caboose(color,x,y);
-         x += 150;
+         x += 175;
          train.add(pos,cab);
          }
       if (type.equals("PassengerCar")) {
          PassengerCar pass = new PassengerCar(color,x,y);
-         x += 150;
+         x += 175;
          train.add(pos,pass);
          }
       if (type.equals("FreightCar")) {
          FreightCar fre = new FreightCar(color,x,y);
-         x += 150;
+         x += 175;
          train.add(pos,fre);}  
    }
-
+   
+   public void addCar(String type,Color color)
+   {
+      if (type.equals("Locomotive")) {
+         Locomotive loco = new Locomotive(color,x,y);
+         x += 175;
+         train.add(loco);
+         }
+      if (type.equals("Caboose")) {
+         Caboose cab = new Caboose(color,x,y);
+         x += 175;
+         train.add(cab);
+         }
+      if (type.equals("PassengerCar")) {
+         PassengerCar pass = new PassengerCar(color,x,y);
+         x += 175;
+         train.add(pass);
+         }
+      if (type.equals("FreightCar")) {
+         FreightCar fre = new FreightCar(color,x,y);
+         x += 175;
+         train.add(fre);
+      }
+      
+   }
+   
+   
+   
    public void showCars(Graphics g)
    {
-     for(RailCar show: train)
-     {
-       show.drawCar(g);
-     }
+      for(RailCar show: train)
+         show.drawCar(g);
+    
    }
   
 }
